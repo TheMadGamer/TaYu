d4f1e2faf26a08eda39635dd2e50aed1cf7bbb1c
Anthony Lobay <lobay@google.com>
2013-08-13 09:27:57 -0700

    Tiles moving around

--- Assets/GameAssets/Scripts/Controllers/GameController.cs
+++ Assets/GameAssets/Scripts/Controllers/GameController.cs
@@ -16,6 +16,8 @@ public class GameController : MonoBehaviour {
 	List<Domino> mDominos = new List<Domino>();
     List<Bag> mBags;
 	
+	GameObject mActiveDomino = null;
+	
     String mPlayer1Name = "Player 1";
     String mPlayer2Name = "Player 2";
 	
@@ -68,7 +70,11 @@ public class GameController : MonoBehaviour {
             mBags.Add(bag);
         }
 
-
+	    mActiveDomino = GamePlayManager.Instance.GetNextDomino();
+		Domino activeController = mActiveDomino.GetComponent<Domino>();
+		mDominos.Add(activeController);
+        activeController.EnableGraphics(mBoard.Controller.Size);
+        activeController.SetHighlight(HighLightMode.Active);
 	}
 	
 	void InitializeDisplay() {
@@ -97,7 +103,12 @@ public class GameController : MonoBehaviour {
 	}
 
 	void Update () {
-
+		Vector2 inputPos = Input.mousePosition;
+		if (Input.GetMouseButtonDown(0)) {
+			MoveDominoToPoint(mActiveDomino, inputPos);
+		} else if (Input.GetMouseButton(0)) {
+			MoveDominoToPoint(mActiveDomino, inputPos);			
+		}
 	}
 	 
 	///<summary>
@@ -142,5 +153,116 @@ public class GameController : MonoBehaviour {
     {
 		// TODO
 	}
+	
+	
+	/// <summary>
+    /// Is a logical row, col, ornt on the board
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="column"></param>
+    /// <param name="isHorizontal"></param>
+    bool LocationIsInBoard(int row, int column, bool isHorizontal)
+    {
+        return (isHorizontal && (-1 < row && row < mBoard.Controller.Size) && (0 < column && column < (mBoard.Controller.Size - 1)))
+                || (!isHorizontal && (0 < row && row < (mBoard.Controller.Size - 1)) && (-1 < column && column < mBoard.Controller.Size));
+    }
 
+    /// <summary>
+    /// Is a logical row, col, ornt in a bag
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="column"></param>
+    /// <param name="isHorizontal"></param>
+    /// <returns></returns>
+    bool LocationIsInUpperBag(int row, int column, bool isHorizontal)
+    {
+        return (isHorizontal && (mBoard.Controller.Size <= row && row < (mBoard.Controller.Size + 2)) && (0 < column && column < (mBoard.Controller.Size - 1)))
+                || (!isHorizontal && ((mBoard.Controller.Size - 1) <= row && row < (mBoard.Controller.Size + 2)) && (-1 < column && column < mBoard.Controller.Size));
+    }
+
+    /// <summary>
+    /// Is a logical row, col, ornt in a bag
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="column"></param>
+    /// <param name="isHorizontal"></param>
+    /// <returns></returns>
+    bool LocationIsInLeftBag(int row, int column, bool isHorizontal)
+    {
+        return (isHorizontal && (-1 < row && row < mBoard.Controller.Size) && (-5 < column && column < 1))
+                || (!isHorizontal && (0 < row && row < (mBoard.Controller.Size - 1)) && (-6 < column && column < 0));
+    }
+
+    /// <summary>
+    /// Gets bag assoc'd with current player
+    /// </summary>
+    /// <returns></returns>
+    protected Bag GetActiveBag()
+    {
+        if (GamePlayManager.Instance.Player1Playing)
+        {
+            return mBags[0];
+        }
+        else
+        {
+            return mBags[1];
+        }
+    }
+
+        ///<summary>
+    /// Move the domino to the closest position next to the Mouse Pointer
+    ///</summary>
+    /// <param name="domino"></param>
+    /// <param name="mousePositionX"></param>
+    /// <param name="mousePosittionY"></param>    
+    void MoveDominoToPoint(GameObject dominoObject, Vector2 mousePoint)
+    {
+		Debug.Log("Move domino to point " + mousePoint.ToString());
+        int row;
+        int column;
+
+        //find row column from point
+        if (mBoard.Controller.Size % 2 == 1)
+        {
+            row = (int)Math.Round((mousePoint.y - 0.5f * Square.kPixelSize) / Square.kPixelSize + 0.5f * mBoard.Controller.Size);
+            column = (int)Math.Round((mousePoint.x - 0.5f * Square.kPixelSize) / Square.kPixelSize + 0.5f * mBoard.Controller.Size);
+
+        }
+        else
+        {
+            row = (int)Math.Round(mousePoint.y / Square.kPixelSize + 0.5f * mBoard.Controller.Size);
+            column = (int)Math.Round(mousePoint.x / Square.kPixelSize + 0.5f * mBoard.Controller.Size);
+        }
+
+		// Note ACL there was some layer logic here.
+		
+		Domino domino = dominoObject.GetComponent<Domino>();
+		
+        // this will move on the board
+        if (LocationIsInBoard(row, column, domino.Controller.IsHorizontal()))
+        {
+            domino.Controller.Row = row;
+            domino.Controller.Column = column;
+            domino.UpdateDominoLocation(mBoard.Controller.Size);
+
+        }
+        
+        // if p1, allow move out top to bag
+        else if (GamePlayManager.Instance.Player1Playing &&
+            LocationIsInUpperBag(row, column, domino.Controller.IsHorizontal()))
+        {
+            domino.Controller.Row = row;
+            domino.Controller.Column = column;
+            domino.UpdateDominoLocation(mBoard.Controller.Size);
+
+        }
+        // if p1, allow move out left to bag
+        else if ((!GamePlayManager.Instance.Player1Playing) &&
+            LocationIsInLeftBag(row, column, domino.Controller.IsHorizontal()))
+        {
+            domino.Controller.Row = row;
+            domino.Controller.Column = column;
+            domino.UpdateDominoLocation(mBoard.Controller.Size);
+        }
+    }
 }
